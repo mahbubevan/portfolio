5ac956485340050f1361a83c43c19ab3664ae810
Evan <evan@Evans-MacBook-Air.local>
2021-09-27 03:05:45 +0600

    Fs problem Solved

--- pages/project.js
+++ pages/project.js
@@ -3,22 +3,43 @@ import Image from 'next/image'
 import utilStyle from '../styles/utils.module.css'
 import { projectLists } from '../lib/project'
 
-export default function Home() {
 
+export async function getStaticProps()
+{
+    const lists = await projectLists()
+    return {
+        props:{
+            lists
+        }
+    }
+}
+
+export default function Home({lists}) {
+  console.log(lists);
   return (
     <Layout>
         <div className='container'>
-          <div className={`card ${utilStyle.cardWidth} bg-dark text-white shadow-lg p-3 mb-5  rounded`}>
-          {/* <Image 
+          <div className="row">
+          {
+            lists.fileObject.map((val,id)=>{
+              return (
+                <div className="col-md-4">
+                    <div key={id} className={`card ${utilStyle.cardWidth} bg-dark text-white shadow-lg p-3 mb-5  rounded`}>
+                    {/* <Image 
 
-          /> */}
-          <div className="card-body">
-            <h5 className="card-title">Project Name</h5>
-            <p className="card-text">
-              Project Description
-            </p>
-            <a href="#" className="btn btn-primary">Project Link</a>
-          </div>
+                    /> */}
+                    <div className="card-body">
+                      <h5 className="card-title">{val[1].projectName}</h5>
+                      <p className="card-text">
+                      {val[1].about}
+                      </p>
+                      <a href="#" className="btn btn-primary">Project Link</a>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
           </div>
         </div>
     </Layout>
